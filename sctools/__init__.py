6f53962407cfabda29ebfb4a874ca6b91ca7c6b3
sin <damon031@icloud.com>
2022-07-03 17:02:56 -0400

    Delete sctools directory

--- sctools/__init__.py
+++
@@ -1,9 +0,0 @@
-from .sctools import SCTools
-
-___red_end_user_data_statement__ = (
-    "This cog only stores an api key from the bot owner"
-)
-
-
-async def setup(bot):
-    bot.add_cog(SCTools(bot))
