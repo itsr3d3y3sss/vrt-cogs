e2e33998f9a5282a86eaea8a426e9a95365aa340
sin <damonbrown44@yahoo.com>
2022-07-03 17:01:56 -0400

    Delete ecotools directory

--- ecotools/__init__.py
+++
@@ -1,9 +0,0 @@
-from .ecotools import EcoTools
-
-___red_end_user_data_statement__ = (
-    "This cog does not persistently store data about users."
-)
-
-
-def setup(bot):
-    bot.add_cog(EcoTools(bot))
